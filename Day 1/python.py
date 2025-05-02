25203b6f23d7a44ab03c0a42bff295bc42c1070b
Ashok <dhakalashok0@gmail.com>
2025-05-02 16:11:44 +0545

    day 1

--- Day 1/python.py
+++ Day 1/python.py
@@ -1,40 +1,21 @@
-# def greet_user(name):
-#     message = "Hello " + name + "!"
-#     return message
-
-# user_name = "Ram Krishna"
-# greeting = greet_user(user_name)
-
-# print(greeting)
-
-#Basic Data Types
-age=21                      #int
-height=5.9                  #float
-name="Ram Krishna"          #string
-is_student=True             #boolean
-
-#control ctructures
-#conditionals
-if age>18:
-    print("You are an adult")
-elif age==18:
-    print("You are an adult")
+age = 21
+height = 5.9
+name = "Alice"
+is_student = True
+if age > 18:
+    print("Adult")
+elif age == 18:
+    print("You just became an adult")
 else:
-    print("You are not an adult")
-
-#loops
+    print("Minor")
 for i in range(5):
     print(f"Number: {i}")
 
-#while loop
-count =0
-while count<3:
-    print("Counting:", count)
-    count+=1
-
-#functions
+    count = 0
+    while count < 3:
+        print(count)
+        count += 1
 def greet(name):
-    return f"Hello {name}!"
+    return f"Hello, {name}!"
 
-#function call
-print(greet("Ram Krishna"))
+print(greet(name))
