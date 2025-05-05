87c8929029325e12cfdd26472000abd5a8ff5578
Ashok <dhakalashok0@gmail.com>
2025-05-05 17:08:50 +0545

    day 3

---
+++ Day 3/operators.py
@@ -0,0 +1,27 @@
+# x = 5
+# x += 5
+# print(x)
+
+# x = 5
+# x -= 5
+# print(x)
+
+# x = 5
+# x *= 5
+# print(x)
+
+# x = 5
+# x /= 5
+# print(x)
+
+x=11
+x//=3
+print(x)
+
+# x=100
+# x%=3
+# print(x)
+
+# x=10
+# x**=2
+# print(x)
