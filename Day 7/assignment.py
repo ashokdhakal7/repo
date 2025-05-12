f48608202b3fe38098b95f1a0594a58c51575e19
Ashok <dhakalashok0@gmail.com>
2025-05-12 16:24:06 +0545

    day 7

--- Day 7/assignment.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def print_presentations():
-    presenters = ['Bikash', 'Sneha', 'Kiran', 'Nabina', 'Suresh', 'Pooja', 'Dipesh', 'Asmita', 'Manoj', 'Ritika']
-    random.shuffle(presenters)
-    for day, presenter in enumerate(presenters, start=1):
-        print(f"{presenter} has presentation on day {day}")
-
-print_presentations()
